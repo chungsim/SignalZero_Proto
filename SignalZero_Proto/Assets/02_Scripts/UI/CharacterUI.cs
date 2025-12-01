9cad1e64d954ad7da43cb94213d0a7ac60cbca26
songduckhee <vhaqjvl1@gmail.com>
2025-12-01 16:34:10 +0900

    feat: 게임스타트 ui 옵션 추가, 필드 추가, 필드메테리얼 추가, ui관련 스크립트추가

---
+++ SignalZero_Proto/Assets/02_Scripts/UI/CharacterUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterUI : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField] private Image healthbarBlue;
+    [SerializeField] private Image healthbarYellow;
+
+    private float baseDistance;
+    private Vector3 baseOffset;
+
+    public Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseDistance = Vector3.Distance(camera.transform.position, player.transform.position);
+        baseOffset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       CalculateRotate();
+    }
+
+    void CalculateRotate()
+    {
+		transform.rotation = Quaternion.LookRotation(player.position - camera.transform.position);
+		Vector3 euler = transform.rotation.eulerAngles;
+		euler.z = 0f;
+		transform.rotation = Quaternion.Euler(euler);
+	}
+}
