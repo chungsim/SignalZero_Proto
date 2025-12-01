d454b9e07ee93b0ea7f22851bd5ce6d5ba6e3b93
juwon <zoowon00@naver.com>
2025-12-01 11:28:14 +0900

    feat : Monster FSM

---
+++ SignalZero_Proto/Assets/98_ZoowonTemp/Scripts/Cs/MonsterBaseState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonsterBaseState : MonoBehaviour
+{
+    public abstract void OnStateEnter();
+    public abstract void OnStateUpdate();
+    public abstract void OnStateExit();
+}
