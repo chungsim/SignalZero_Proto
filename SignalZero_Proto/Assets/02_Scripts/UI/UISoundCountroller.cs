8313f45ddb64cde994f07f3f8567fa23e10dee43
songduckhee <vhaqjvl1@gmail.com>
2025-12-04 19:08:00 +0900

    feat: 메테리얼, 옵션 구현 작업 끝

---
+++ SignalZero_Proto/Assets/02_Scripts/UI/UISoundCountroller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public enum VolumeType
+{
+	Master,
+	BGM,
+	SFX
+}
+public class UISoundCountroller : MonoBehaviour
+{
+	public Slider soundSlider;
+	public Image volume;
+	private float sliderValue;
+
+	public AudioMixer masterMixer;
+	public VolumeType volumeType;
+	// Start is called before the first frame update
+	void Start()
+	{
+		soundSlider.onValueChanged.AddListener(OnSliderValueChanged);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnSliderValueChanged(float value)
+	{
+		volume.fillAmount = value * 0.1f;
+		switch (volumeType)
+		{
+			case VolumeType.Master:
+				float volume = Mathf.Log10(Mathf.Clamp(value, 0.0001f, 1f)) * 20f;
+				masterMixer.SetFloat("MasterParameter", volume);
+				break;
+			case VolumeType.BGM:
+				//GameManager.Instance.audioManager.bgmSource.volume = value;
+				break;
+			case VolumeType.SFX:
+				//GameManager.Instance.audioManager.sfxSource.volume = value;
+				break;
+			default:
+				break;
+		}
+		
+	}
+
+
+}
